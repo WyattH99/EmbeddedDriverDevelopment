10cbe8eaed2824deed675871940329fe680e3691
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-02-12 13:17:56 -0600

    008_stm32f4xx_drivers Completed 002_Led_Button.c

--- 008_stm32f4xx_drivers/Debug/Src/002_Led_Button.d
+++ 008_stm32f4xx_drivers/Debug/Src/002_Led_Button.d
@@ -1,4 +1,4 @@
-Src/001_Led_Toggle.o: ../Src/001_Led_Toggle.c \
+Src/002_Led_Button.o: ../Src/002_Led_Button.c \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx.h \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx_gpio_driver.h \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx.h
--- 008_stm32f4xx_drivers/Debug/Src/subdir.mk
+++ 008_stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/001_Led_Toggle.c \
+../Src/002_Led_Button.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/001_Led_Toggle.o \
+./Src/002_Led_Button.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/001_Led_Toggle.d \
+./Src/002_Led_Button.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/001_Led_Toggle.cyclo ./Src/001_Led_Toggle.d ./Src/001_Led_Toggle.o ./Src/001_Led_Toggle.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/002_Led_Button.cyclo ./Src/002_Led_Button.d ./Src/002_Led_Button.o ./Src/002_Led_Button.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
