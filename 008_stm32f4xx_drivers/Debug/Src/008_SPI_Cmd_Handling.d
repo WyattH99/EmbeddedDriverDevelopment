8b5b27e0f28c69f8cb6db0b3c7f61d68408d9b47
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-02-20 11:19:21 -0600

    008_stm32f4xx_drivers Completed 008_SPI_Cmd_Handling

--- 008_stm32f4xx_drivers/Debug/Src/008_SPI_Cmd_Handling.d
+++ 008_stm32f4xx_drivers/Debug/Src/008_SPI_Cmd_Handling.d
@@ -1,4 +1,4 @@
-Src/007_SPI_TxOnly_Arduino.o: ../Src/007_SPI_TxOnly_Arduino.c \
+Src/008_SPI_Cmd_Handling.o: ../Src/008_SPI_Cmd_Handling.c \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx.h \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx_gpio_driver.h \
  /home/wyatt/Desktop/EmbeddedDriverDevelopment/008_stm32f4xx_drivers/Drivers/Inc/stm32f446xx.h \
--- 008_stm32f4xx_drivers/Debug/Src/subdir.mk
+++ 008_stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/007_SPI_TxOnly_Arduino.c \
+../Src/008_SPI_Cmd_Handling.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/007_SPI_TxOnly_Arduino.o \
+./Src/008_SPI_Cmd_Handling.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/007_SPI_TxOnly_Arduino.d \
+./Src/008_SPI_Cmd_Handling.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/007_SPI_TxOnly_Arduino.cyclo ./Src/007_SPI_TxOnly_Arduino.d ./Src/007_SPI_TxOnly_Arduino.o ./Src/007_SPI_TxOnly_Arduino.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/008_SPI_Cmd_Handling.cyclo ./Src/008_SPI_Cmd_Handling.d ./Src/008_SPI_Cmd_Handling.o ./Src/008_SPI_Cmd_Handling.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
